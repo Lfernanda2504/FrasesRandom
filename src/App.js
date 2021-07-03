f0ea85c33fd5b17234a188f04ada6247719b9d06
lfernanda2504 <luizafernandagarcia@gmail.com>
2021-07-02 19:56:51 -0500

    archivos

--- src/App.js
+++ src/App.js
@@ -1,11 +1,55 @@
-
+import React, { useState, useEffect } from 'react';
 import './App.css';
-import QuoteMachine from './components/QuoteMachine';
+import { FiTwitter } from 'react-icons/fi';
+
 
 function App() {
+  const [quote, setQuote] = useState(" ");
+  const [author, setAuthor] = useState(" ");
+
+  // http://api.quotable.io/random
+  useEffect(() => {
+    fetch("http://api.quotable.io/random")
+      .then(res => res.json())
+      .then(
+        (quote) => {
+          setQuote(quote.content);
+          setAuthor(quote.author);
+          console.log(quote);
+        }
+      )
+  }, []);
+  let NewQuote = () => {
+    fetch("http://api.quotable.io/random")
+      .then(res => res.json())
+      .then(
+        (quote) => {
+          setQuote(quote.content);
+          setAuthor(quote.author);
+        }
+      )
+  }
   return (
     <div>
-      <QuoteMachine/>
+      <h1 className='title'>Random Quote App</h1>
+      <div className="container">
+        <div className="quote" id="quote-box">
+          <div id="text">{quote}</div>
+          <small id="author">-{author}-</small>
+        </div>
+        <div className="container-fluid text-center">
+          <div className="row">
+            <div className="col-6 col-ms-3">
+            <button id="new-quote" onClick={NewQuote}>Generate New Quote</button>
+            </div>
+            <div className="col-6 col-ms-3">
+            <a  id="tweet-quote" href={`https://twitter.com/intent/tweet?text=${quote} -${author}-`} target="_blank">
+            <FiTwitter></FiTwitter>
+              </a>
+            </div>
+          </div>
+        </div>
+      </div>
     </div>
   );
 }
